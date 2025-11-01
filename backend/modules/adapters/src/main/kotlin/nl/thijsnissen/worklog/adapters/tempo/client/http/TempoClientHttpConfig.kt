12fa0f6326b4875b24ed60e091cb74cbfbab45d3
Thijs Nissen <thijsnissen@gmail.com>
2025-11-01 16:55:11 +0100

    proposes fix for rate limiting issues in tempo api

--- backend/modules/adapters/src/main/kotlin/nl/thijsnissen/worklog/adapters/tempo/client/http/TempoClientHttpConfig.kt
+++ backend/modules/adapters/src/main/kotlin/nl/thijsnissen/worklog/adapters/tempo/client/http/TempoClientHttpConfig.kt
@@ -1,10 +1,12 @@
 package nl.thijsnissen.worklog.adapters.tempo.client.http
 
 import java.net.URI
+import java.time.Duration
 
 data class TempoClientHttpConfig(
     val host: URI,
     val apiKey: String,
     val accountId: String,
     val maxConcurrentRequests: Int,
+    val minRequestInterval: Duration,
 )
