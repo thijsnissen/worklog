bb4a6a5b6ecb1eac0bcbdf17a57b48d6ae9c49c1
Thijs Nissen <617073+thijsnissen@users.noreply.github.com>
2026-04-01 20:26:12 +0200

    Fail on required configuration

--- backend/modules/adapters/src/main/kotlin/nl/thijsnissen/worklog/adapters/jira/client/http/JiraClientHttpConfig.kt
+++ backend/modules/adapters/src/main/kotlin/nl/thijsnissen/worklog/adapters/jira/client/http/JiraClientHttpConfig.kt
@@ -2,4 +2,9 @@ package nl.thijsnissen.worklog.adapters.jira.client.http
 
 import java.net.URI
 
-data class JiraClientHttpConfig(val host: URI, val userEmail: String, val apiKey: String)
+data class JiraClientHttpConfig(val host: URI, val userEmail: String, val apiKey: String) {
+    init {
+        require(userEmail.isNotBlank()) { "Field userEmail cannot be blank." }
+        require(apiKey.isNotBlank()) { "Field apiKey cannot be blank." }
+    }
+}
